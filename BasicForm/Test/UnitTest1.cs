1021c0594c11c18476819ab27b13a1efff077c49
Jirka Vrbka <vrbka.jirka@gmail.com>
2017-04-18 19:47:48 +0200

    ABC

---
+++ BasicForm/Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace BasicForm.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
